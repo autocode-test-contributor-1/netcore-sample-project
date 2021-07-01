54d5bbe0434a36407e4ccc77d25e8f39559f73de
Kirill Peshin <kirill_peshin@epam.com>
2021-07-01 08:35:58 +0300

    Upd

--- FancyCalc/FancyCalcEnguine.cs
+++ FancyCalc/FancyCalcEnguine.cs
@@ -4,25 +4,26 @@ namespace FancyCalc
 {
     public class FancyCalcEnguine
     {
+        public IStorage Storage { get; set; }
 
         public double Add(int a, int b)
         {
-            throw new NotImplementedException();
-            //return a + b;
+//             throw new NotImplementedException();
+            return a + b;
         }
 
 
         public double Subtract(int a, int b)
         {
-            throw new NotImplementedException();
-           // return a - b;
+//             throw new NotImplementedException();
+           return a - b;
         }
 
 
         public double Multiply(int a, int b)
         {
-            //return a * b;
-             throw new NotImplementedException();
+            return a * b;
+//              throw new NotImplementedException();
             
         }
     }
